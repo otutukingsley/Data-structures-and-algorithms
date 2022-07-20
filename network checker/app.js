42245d6e04dfa0bbe0ca8e32dd0ebca30e6dd57e
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2022-07-20 13:50:43 +0100

    network checker

---
+++ network checker/app.js
@@ -0,0 +1,26 @@
+// Network Checker
+// an Object with an array of the substrings of each network
+// Inputed number
+
+let networks = {
+  mtn: ["0803", "0703", "0903", "0806", "0706", "0813", "0810", "0814", "0816"],
+  airtel: ["0802", "0902", "0701", "0808", "0708", "0812"],
+  global: ["0805", "0705", "0905", "0807", "0811", "0815", "0905"],
+  etisalat: ["0809", "0909", "0817", "0818"],
+}
+
+function networkCheck(number) {
+  let numberArray = number.split("")
+  let numberString = numberArray.join("")
+  let network = ""
+  for (let key in networks) {
+    for (let i = 0; i < networks[key].length; i++) {
+      if (numberString.includes(networks[key][i])) {
+        network = key
+      }
+    }
+  }
+  return network
+}
+
+console.log(networkCheck("07056471853"))
