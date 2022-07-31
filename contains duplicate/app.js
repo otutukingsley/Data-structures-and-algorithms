e6ea2f0b018dc49f60f7c28461d0f24662fa0766
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2022-07-31 08:51:32 +0100

    contains duplicate solution two

--- contains duplicate/app.js
+++ contains duplicate/app.js
@@ -19,3 +19,15 @@ var containsDuplicate = function (nums) {
 
 //Solution 2 
 
+var containsDuplicate = function (nums) {
+  let sorted = nums.sort((a, b) => a - b)
+
+  for (let i = 0; i < sorted.length; i++) {
+
+    if (sorted[i] === sorted[i + 1]) {
+      return true
+    }
+  }
+
+  return false
+};
