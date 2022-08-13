085a3bff544403608467bafc856ec350283554af
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2022-08-13 10:15:14 +0100

    range sum query - immutable

---
+++ range sum query - immutable/app.js
@@ -0,0 +1,49 @@
+// Given an integer array nums, handle multiple queries of the following type:
+
+// Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+// Implement the NumArray class:
+
+// NumArray(int[] nums) Initializes the object with the integer array nums.
+// int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+
+
+// Example 1:
+
+// Input
+// ["NumArray", "sumRange", "sumRange", "sumRange"]
+// [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+// Output
+// [null, 1, -1, -3]
+
+// Explanation
+// NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+// numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+// numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+// numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+
+
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+  nums.forEach((item, index) => {
+    nums[index] = (nums[index - 1] || 0) + item
+  })
+
+  this.nums = nums
+};
+
+/** 
+* @param {number} left 
+* @param {number} right
+* @return {number}
+*/
+NumArray.prototype.sumRange = function (left, right) {
+  return this.nums[right] - (this.nums[left - 1] || 0)
+};
+
+/** 
+* Your NumArray object will be instantiated and called as such:
+* var obj = new NumArray(nums)
+* var param_1 = obj.sumRange(left,right)
+*/
