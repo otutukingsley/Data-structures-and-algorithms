46efa9a81308781b99e7518eca45187f0bc61fb5
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2022-08-14 01:27:03 +0100

    counting bits

--- counting bits/app.js
+++ counting bits/app.js
@@ -22,26 +22,64 @@
 // 4 --> 100
 // 5 --> 101
 
-var countOnes = function (data) {
-  let count = 0
-  for (let i = 0; i < data.length; i++) {
-    if (data[i] == '1') {
-      count++
-    }
-  }
+// var countOnes = function (data) {
+//   let count = 0
+//   for (let i = 0; i < data.length; i++) {
+//     if (data[i] == '1') {
+//       count++
+//     }
+//   }
 
-  return count
-}
+//   return count
+// }
 
 
-var countBits = function (n) {
-  let result = []
-  let numLength = n + 1
+// var countBits = function (n) {
+//   let result = []
+//   let numLength = n + 1
 
-  for (let i = 0; i < numLength; i++) {
-    const parsed = countOnes(i.toString(2))
-    result.push(parsed)
-  }
+//   for (let i = 0; i < numLength; i++) {
+//     const parsed = countOnes(i.toString(2))
+//     result.push(parsed)
+//   }
 
-  return result
-};
+//   return result
+// };
+
+
+
+// var nextGreatestLetter = function (letters, target) {
+//   let left = 0
+//   let right = letters.length - 1
+//   let nextLetter = letters[left]
+//   let mid = Math.floor((left + right) / 2)
+
+//   while (left <= right) {
+//     if (target < letters[mid]) {
+//       nextLetter = letters[mid]
+//       right = mid - 1
+//     } else {
+//       left = mid + 1
+//     }
+//     mid = Math.floor((left + right) / 2)
+//   };
+
+//   return nextLetter
+// }
+// console.log(nextGreatestLetter(['c', 'f', 'j'], 'i'))
+
+
+
+// function balancedParens(n) {
+//   if (n === 0) return [""]
+
+//   let parenArr = []
+
+//   for (let i = 0; i < n; i++) {
+//     parenArr.push('()')
+//   }
+
+//   console.log(parenArr.join(''))
+// }
+
+// balancedParens(4)
