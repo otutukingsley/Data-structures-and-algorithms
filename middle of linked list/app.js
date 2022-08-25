7e66a1165b7d9606fb2543f1965a6b1cc3a8ce4a
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2022-08-25 01:33:33 +0100

    middle of linked list

---
+++ middle of linked list/app.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let current = head;
+  let counter = 0;
+  let middle;
+  let mover = head;
+
+  while (current) {
+    current = current.next;
+    counter++;
+  }
+
+  if (counter % 2 === 0) {
+    middle = Math.ceil(counter / 2);
+  } else {
+    middle = Math.floor(counter / 2);
+  }
+
+  for (let i = 0; i < middle; i++) {
+    mover = mover.next;
+  }
+  return mover;
+};
