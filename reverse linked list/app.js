4ad3d635549164b3e9e3308c772d315f6aa18abd
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2022-09-29 01:51:23 +0100

    added descriptions

--- reverse linked list/app.js
+++ reverse linked list/app.js
@@ -9,6 +9,11 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+//Description
+
+//Given the head of a singly linked list, reverse the list, and return the reversed list.
+
 var reverseList = function(head) {
   let current = head
   let previous = null
