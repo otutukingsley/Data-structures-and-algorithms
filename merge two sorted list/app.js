c5517827f676f7936d7b9fea7310fafb98d9c686
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2022-10-01 01:53:59 +0100

    added descriptions

--- merge two sorted list/app.js
+++ merge two sorted list/app.js
@@ -10,6 +10,13 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
+
+//Description
+//You are given the heads of two sorted linked lists list1 and list2.
+//Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+//Return the head of the merged linked list.
+
+
 var mergeTwoLists = function(list1, list2) { 
   const dummy = new ListNode(-Infinity)
   let prev = dummy
