4cc97f2235d0343f7648aff1dd40846871909fdc
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2022-10-07 02:15:39 +0100

    binary search second solution

--- binary search/app.js
+++ binary search/app.js
@@ -14,3 +14,23 @@ var search = function(nums, target) {
   
   return -1
 };
+
+
+var search2 = function(nums, target){
+  let lo = 0; // 3
+  let hi = nums.length - 1 // 5
+  
+  while(lo < hi){
+      let mid = lo + Math.floor((hi - lo + 1) / 2)
+      
+      if(target < nums[mid]){
+          hi = mid - 1 
+      }else {
+          lo = mid
+      }
+      
+  }
+  
+  return nums[lo] === target ? lo : -1 
+  
+}
