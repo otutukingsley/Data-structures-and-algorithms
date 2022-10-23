f00e7f85d960edf2faa2fbb02997f8fee0556608
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2022-10-23 03:09:10 +0100

    same tree

---
+++ binaryTree/sametree.js
@@ -0,0 +1,38 @@
+var isSameTree = function(p, q) { 
+  if(!p && !q) return true
+  if(!p || !q) return false
+  
+  let plist = [p]
+  let qlist = [q]
+  
+  while(plist.length > 0) {
+      let plist_length = plist.length
+      for(let i = 0; i < plist_length; i++){
+          let p_top = plist.shift()
+          let q_top = qlist.shift()
+          
+          if(p_top.val !== q_top.val) return false
+          if(!p_top || !q_top) return false
+          
+          if((p_top.left && !q_top.left) || (q_top.left && !p_top.left)){
+              return false
+          } else {
+              if(p_top.left && q_top.left){
+                  plist.push(p_top.left)
+                  qlist.push(q_top.left)
+              }
+          }
+          
+          if((p_top.right && !q_top.right) || (q_top.right && !p_top.right)){
+              return false
+          } else {
+              if(p_top.right && q_top.right){
+                  plist.push(p_top.right)
+                  qlist.push(q_top.right)
+              }
+          }
+      }
+  }
+  
+  return plist.length === qlist.length
+};
