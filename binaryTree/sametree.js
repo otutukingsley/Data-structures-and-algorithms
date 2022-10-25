5598dc469c9075786cde89c86fbf801524858e3a
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2022-10-25 03:10:03 +0100

    added descriptions

--- binaryTree/sametree.js
+++ binaryTree/sametree.js
@@ -1,3 +1,10 @@
+//Description
+
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+
 var isSameTree = function(p, q) { 
   if(!p && !q) return true
   if(!p || !q) return false
