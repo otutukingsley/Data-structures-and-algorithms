4f91a6a44e4c5117cb13ea60e4389739f61d787c
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2022-12-18 03:40:57 +0100

    added description

--- setMatrixZeroes.js
+++ setMatrixZeroes.js
@@ -1,3 +1,6 @@
+//Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+
 var setZeroes = function (matrix) {
   let rowSet = new Set();
   let colSet = new Set();
